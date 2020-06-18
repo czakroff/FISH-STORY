2108584dac2ef5828b4e138c8f072e8e5d669c53
Casey Zakroff <czakroff@mit.edu>
2020-06-17 21:50:17 -0400

    Coded for Obi Wolf Tutorial
    Basic system functionality for running the ObiTools3 wolf tutorial. Still lacking summary table of results and Dash app.

---
+++ data-processing/proc_utils.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+'''
+Written for Insight Data Engineering Fellowship
+Version 1.0: Wolf tutorial test 
+Casey Zakroff; Jun 17, 2020
+'''
+
+### Libraries
+import numpy as np
+import pandas as pd
+
+#wolf version: Converts fastQ files to pandas dataframe
+def pandas_to_fastQ(df, fwd_path, rev_path):
+
+	#Create writeable files
+    with open(fwd_path, 'w') as fwd:
+        with open(rev_path, 'w') as rev:
+        
+        	#loop down table, writing rows to file 
+            for i in range(0,len(df)):
+                fwd.write(df.loc[i,'SRA'])
+                rev.write(df.loc[i,'SRA'])
+                
+                fwd.write(df.loc[i,'read_F'])
+                rev.write(df.loc[i,'read_R'])
+                
+                fwd.write('+\n')
+                rev.write('+\n')
+                
+                fwd.write(df.loc[i,'qual_F'])
+                rev.write(df.loc[i,'qual_R'])
+
+#Convert fasta to dicts
+def fasta_to_dicts(fasta_path):
+    dict_list = list()
+    tuples = []
+
+    with open(fasta_path) as result:
+        for line in result:
+            if (line[0] == '>'):
+                split_line = line.split(';')
+                
+                for i in range(0,len(split_line)-1):
+                    #ID
+                    if i == 0:
+                        tuples.append(('ID',split_line[0][1:]))
+                        
+                    #
+                    else:
+                        fields = split_line[i].split('=')
+                        tuples.append((fields[0],fields[1]))
+                        
+                dict_list.append(dict(tuples))
+                tuples = []
+                
+    return dict_list
--- ingestion/fastq_utils.py
+++
@@ -1,7 +0,0 @@
-
-
-#Convert FastQ to table
-
-#Convert table to FastQ
-
-#Convert Fasta ot table
